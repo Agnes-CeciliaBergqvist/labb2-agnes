7b92183afa932e823f312084cea0f9ab065fb0bd
Agnes Bergqvist <agnes.bergqvist@hotmail.com>
2021-09-14 10:23:39 +0200

    Started on my own plugin

---
+++ wp-content/plugins/my-own-plugin/my-own-plugin.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: My own plugin
+ * Plugin URI: http://www.medieinstitutet.se
+ * Description: My own plugin for Labb2
+ * Version: 1.0
+ * Author: Agnes-Cecilia Bergqvist
+ */
