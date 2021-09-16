cdc1213629583b620b91da380286a556895d26f3
Agnes Bergqvist <agnes.bergqvist@hotmail.com>
2021-09-16 11:05:45 +0200

    SOverride template done with title and started with css override

--- wp-content/themes/shopstore-child/functions.php
+++ wp-content/themes/shopstore-child/functions.php
@@ -4,6 +4,13 @@
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
     
+    
+}
+// Creating my own css override
+function my_custom_style() {
+    wp_register_style('custom-styles', get_template_directory_uri(). '/style/override.css'); 
+    wp_enqueue_style('custom-styles'); 
+
 }
 // Tar bort och lägger till action. Tar bort Thumbnail bilden på produktsidan. Init gör så att den sätts igång så snart sidan körs. 
 add_action('init', 'remove_some_things');
@@ -13,3 +20,4 @@ function remove_some_things() {
 
 
  add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+ add_action('wp_enqueue_scripts', 'my_custom_style'); 
