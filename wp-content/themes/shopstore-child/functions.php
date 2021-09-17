a061ee839b2fb74e77cbbe68f5df0b676a7ee852
Agnes Bergqvist <agnes.bergqvist@hotmail.com>
2021-09-17 11:37:54 +0200

    Added style override

--- wp-content/themes/shopstore-child/functions.php
+++ wp-content/themes/shopstore-child/functions.php
@@ -3,15 +3,12 @@
 // Fetching parent styles
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    wp_register_style('custom-styles', get_stylesheet_directory_uri(). '/style/override.css'); //stylesheet används vid child style för att få fram det. 
+    wp_enqueue_style('custom-styles', 15); 
     
     
 }
 // Creating my own css override
-function my_custom_style() {
-    wp_register_style('custom-styles', get_template_directory_uri(). 'override.css'); 
-    wp_enqueue_style('custom-styles'); 
-
-}
 // Tar bort och lägger till action. Tar bort Thumbnail bilden på produktsidan. Init gör så att den sätts igång så snart sidan körs. 
 add_action('init', 'remove_some_things');
 function remove_some_things() {
@@ -19,5 +16,4 @@ function remove_some_things() {
 }
 
 
- add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
- add_action('wp_enqueue_scripts', 'my_custom_style'); 
+ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' ); 
