c881f3eb64c15fde3b4114684766a3349d091354
Agnes Bergqvist <agnes.bergqvist@hotmail.com>
2021-09-20 19:22:57 +0200

    Fixed plugin text and chnaged logo

--- wp-content/plugins/my-own-plugin/my-own-plugin.php
+++ wp-content/plugins/my-own-plugin/my-own-plugin.php
@@ -7,12 +7,18 @@
  * Author: Agnes-Cecilia Bergqvist
  */
 
-
-
-function my_own_plugin( $content) {
-    return $content = '<h1>Welcome to New Balance, this is my own plugin!</h1>';
-    
-    
-}
-add_action('the_content' , 'my_own_plugin');
-
+function my_own_plugin($content) {
+  
+         $signature = "<a>Want 10% off next order? (My own plugin)</a>" ;
+   
+         if ( is_singular() || in_the_loop() || is_main_query()) { 
+         
+       // This is a single post 
+                 $content = $content.$signature;
+    }
+      
+         
+    return $content;
+ }
+   
+ add_filter('the_content','my_own_plugin');
--- wp-content/themes/shopstore-child/functions.php
+++ wp-content/themes/shopstore-child/functions.php
@@ -1,6 +1,7 @@
 <?php 
 
 // Fetching parent styles
+// Creating my own css override
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
     wp_register_style('custom-styles', get_stylesheet_directory_uri(). '/style/override.css'); //stylesheet används vid child style för att få fram det. 
@@ -8,7 +9,7 @@ function enqueue_parent_styles() {
     
     
 }
-// Creating my own css override
+
 // Tar bort och lägger till action. Tar bort Thumbnail bilden på produktsidan. Init gör så att den sätts igång så snart sidan körs. 
 add_action('init', 'remove_some_things');
 function remove_some_things() {
